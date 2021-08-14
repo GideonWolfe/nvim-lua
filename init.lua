c0520efc7d77a3def3503da403b3311aa01ed7cb
gideon <gideon@gideonwolfe.com>
2021-08-14 11:14:35 -0700

    init.lua

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Main nvim configuration options
+require('settings')
+-- Plugins
+require('plugins')
+-- Key mappings
+require('mappings')
+-- Colors and highlights
+require('colors')
+-- Configuration for LSPs
+require('lsp.lsp-config')
