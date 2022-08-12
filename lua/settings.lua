0432bcc3f61ff3eb9215dd80fb22c4d5f968fe4c
gideonwolfe <wolfeg2@wwu.edu>
2022-08-11 23:25:47 -0700

    revamped settings thanks to sangularisart

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,10 +1,3 @@
--- Global vim options
-local opt = vim.opt
-
--- If we need to specify (:h <option> for info)
-local global = vim.o
-local window = vim.wo
-local buffer = vim.bo
 -- vim commands
 local cmd = vim.cmd
 -- global vars (let g: stuff)
@@ -12,165 +5,172 @@ local g = vim.g
 
 -- style we can assign to windows
 g.floating_window_border_dark = {
-    {"╭", "FloatBorderDark"},
-    {"─", "FloatBorderDark"},
-    {"╮", "FloatBorderDark"},
-    {"│", "FloatBorderDark"},
-    {"╯", "FloatBorderDark"},
-    {"─", "FloatBorderDark"},
-    {"╰", "FloatBorderDark"},
-    {"│", "FloatBorderDark"}
+	{ "╭", "FloatBorderDark" },
+	{ "─", "FloatBorderDark" },
+	{ "╮", "FloatBorderDark" },
+	{ "│", "FloatBorderDark" },
+	{ "╯", "FloatBorderDark" },
+	{ "─", "FloatBorderDark" },
+	{ "╰", "FloatBorderDark" },
+	{ "│", "FloatBorderDark" },
 }
+-- Don't autosave
+g.auto_save = false
 
 -- automatically compile plugins.lua when editing
 --cmd [[autocmd BufWritePost plugins.lua PackerCompile]]
 
 -- Packer Commands
-cmd [[command! WhatHighlight :call util#syntax_stack()]]
-cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
-cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
-cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
-cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
-cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
-
--- Allow new buffer to be opened without saving current
-opt.hidden = true
-
---  Automatically reread the file if a change was detected outside of vim
-opt.autoread = true
+cmd([[command! WhatHighlight :call util#syntax_stack()]])
+cmd([[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]])
+cmd([[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]])
+cmd([[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]])
+cmd([[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]])
+cmd([[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]])
 
 -- Automatically activate spellcheck for these files
-vim.cmd[[autocmd FileType text,markdown,tex setlocal spell ]]
+vim.cmd([[autocmd FileType text,markdown,tex setlocal spell ]])
 -- Limit line lenght when writing prose
-vim.cmd[[autocmd FileType text,markdown,tex setlocal textwidth=180 ]]
+vim.cmd([[autocmd FileType text,markdown,tex setlocal textwidth=180 ]])
 
+local settings = {
+	-- Allow new buffer to be opened without saving current
+	hidden = true,
+	--  Automatically reread the file if a change was detected outside of vim
+	autoread = true,
+	-- required option for compe autocomplete
+	completeopt = "menuone,noselect",
+	-- don't show mode indicator line
+	showmode = false,
+	-- Enable swap file and location of file
+	swapfile = true,
+	dir = "/tmp",
+	-- Highlight search results
+	hlsearch = true,
+	incsearch = true,
+	-- Case sensitive search unless uppercase is present
+	ignorecase = true,
+	smartcase = true,
+	-- Keep context around cursor
+	scrolloff = 3,
+	sidescrolloff = 5,
+	-- Enable mouse scroll
+	mouse = "a",
+	-- Line Numbers
+	number = true,
+	relativenumber = true,
+	-- Don't automatically collapse markdown and latex
+	conceallevel = 0,
+	-- Set clipboard to system
+	clipboard = "unnamedplus",
+	-- Show matching braces/tags
+	showmatch = true,
+	-- Enable line number of current line
+	-- looking different
+	-- TODO enable line number color while disabling bar
+	cursorline = true,
+	-- Tab settings
+	expandtab = true,
+	tabstop = 2,
+	softtabstop = 2,
+	shiftwidth = 2,
+	smarttab = true,
+	autoindent = true,
+	smartindent = true,
+	shiftround = true,
+	-- Line wrapping
+	wrap = true,
+	-- Split windows to the right
+	splitright = true,
+	-- make statusline cover the entire bottom
+	laststatus = 3,
+	-- creates a backup file
+	backup = false,
+	-- more space in the neovim command line for displaying messages
+	cmdheight = 2,
+	-- the encoding written to a file
+	fileencoding = "utf-8",
+	-- pop up menu height
+	pumheight = 10,
+	-- always show tabs
+	showtabline = 2,
+	-- force all horizontal splits to go below current window
+	splitbelow = true,
+	-- time to wait for a mapped sequence to complete (in milliseconds)
+	timeoutlen = 500,
+	-- enable persistent undo
+	undofile = true,
+	-- faster completion (4000ms default)
+	updatetime = 300,
+	-- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	writebackup = false,
+	-- set number column width to 2 {default 4}
+	numberwidth = 2,
+	-- always show the sign column, otherwise it would shift the text each time
+	signcolumn = "yes",
+}
 
+vim.opt.shortmess:append("c") -- don't show redundant messages from ins-completion-menu
+vim.opt.shortmess:append("I") -- don't show the default intro message
+vim.opt.whichwrap:append("<,>,[,],h,l")
 
--- required option for compe autocomplete
-vim.o.completeopt = "menuone,noselect"
-vim.opt.shortmess:append('c')
-
--- Don't autosave
-g.auto_save = false
-
--- don't show mode indicator line
-opt.showmode = false
-
--- Enable swap file and location of file
-opt.swapfile = true
-opt.dir = '/tmp'
-
--- Highlight search results
-opt.hlsearch = true
-opt.incsearch = true
-
--- Case sensitive search unless uppercase is present
-opt.ignorecase = true
-opt.smartcase = true
-
--- Keep context around cursor
-opt.scrolloff = 3
-opt.sidescrolloff = 5
-
--- Enable mouse scroll
-opt.mouse = 'a'
-
--- Line Numbers
-opt.number = true
-opt.relativenumber = true
-
--- Automatically reload a file if a change was detected outside of vim
-opt.autoread = true
-
--- Don't automatically collapse markdown and latex
-opt.conceallevel = 0
-
--- Set clipboard to system
-opt.clipboard = 'unnamedplus'
-
--- Show matching braces/tags
-opt.showmatch = true
-
--- Enable line number of current line
--- looking different
--- TODO enable line number color while disabling bar
-opt.cursorline = true
-
--- Tab settings
-opt.expandtab = true
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
-opt.smarttab = true
-opt.autoindent = true
-opt.smartindent = true
-opt.shiftround = true
-
--- Line wrapping
-opt.wrap = true
-
--- Split windows to the right
-opt.splitright = true
- 
--- go to previous/next line with h,l,left arrow and right arrow
--- when cursor reaches end/beginning of line
-opt.whichwrap:append("<>hl") 
+for k, v in pairs(settings) do
+	vim.opt[k] = v
+end
 
 -- Don't show status line on vim terminals
-vim.cmd [[ au TermOpen term://* setlocal nonumber laststatus=0 ]]
+vim.cmd([[ au TermOpen term://* setlocal nonumber laststatus=0 ]])
 
 -- Disable some built-in plugins we don't want
 local disabled_built_ins = {
-  'gzip',
-  'man',
-  'matchit',
-  'matchparen',
-  'shada_plugin',
-  'tarPlugin',
-  'tar',
-  'zipPlugin',
-  'zip',
-  'netrwPlugin',
+	"gzip",
+	"man",
+	"matchit",
+	"matchparen",
+	"shada_plugin",
+	"tarPlugin",
+	"tar",
+	"zipPlugin",
+	"zip",
+	"netrwPlugin",
 }
 
 for i = 1, 10 do
-  g['loaded_' .. disabled_built_ins[i]] = 1
+	g["loaded_" .. disabled_built_ins[i]] = 1
 end
-
 
 -- Remember last position in file
-vim.cmd[[autocmd BufReadPost * lua goto_last_pos()]]
-function goto_last_pos()
-  local last_pos = vim.fn.line("'\"")
-  if last_pos > 0 and last_pos <= vim.fn.line("$") then
-    vim.api.nvim_win_set_cursor(0, {last_pos, 0})
-  end
-end
-
-
-
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		vim.cmd([[
+      if line("'\"") > 0 && line("'\"") <= line("$") |
+        exe "normal! g`\"" |
+      endif
+    ]])
+	end,
+})
 
 -- function to set autogroups easily
 -- https://github.com/Th3Whit3Wolf/dots/blob/main/private_dot_config/private_nvim/private_lua/private_autocmd.lua
 local function augroups(definitions)
-    for group_name, definition in pairs(definitions) do
-        vim.api.nvim_command("augroup " .. group_name)
-        vim.api.nvim_command("autocmd!")
-        for _, def in ipairs(definition) do
-            local command = table.concat(vim.tbl_flatten {"autocmd", def}, " ")
-            vim.api.nvim_command(command)
-        end
-        vim.api.nvim_command("augroup END")
-    end
+	for group_name, definition in pairs(definitions) do
+		vim.api.nvim_command("augroup " .. group_name)
+		vim.api.nvim_command("autocmd!")
+		for _, def in ipairs(definition) do
+			local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
+			vim.api.nvim_command(command)
+		end
+		vim.api.nvim_command("augroup END")
+	end
 end
 
 -- Define our autocommands
 local autocmds = {
-    --- Current window has hybrid numbers
-    --- All other windows have absolute numbers
-    numberToggleConditional = {
-        {"BufEnter,FocusGained,InsertLeave", "*", [[if &ft !~# 'dashboard' | set relativenumber | endif]]},
-        {"BufLeave,FocusLost,InsertEnter", "*", [[if &ft !~# 'dashboard' | set norelativenumber | endif]]}
-    },
+	--- Current window has hybrid numbers
+	--- All other windows have absolute numbers
+	numberToggleConditional = {
+		{ "BufEnter,FocusGained,InsertLeave", "*", [[if &ft !~# 'dashboard' | set relativenumber | endif]] },
+		{ "BufLeave,FocusLost,InsertEnter", "*", [[if &ft !~# 'dashboard' | set norelativenumber | endif]] },
+	},
 }
 augroups(autocmds)
