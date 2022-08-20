61282fe39ddb0d8ec2ff3dce2ec5b042d339b351
gideonwolfe <wolfeg2@wwu.edu>
2022-08-20 10:19:26 -0700

    structure

--- lua/lsp/lsp-config.lua
+++ lua/lsp/lsp-config.lua
@@ -94,13 +94,13 @@ require'lspconfig'.sumneko_lua.setup({
       diagnostics = {
         globals = { "vim" },
       },
-    },
-    workspace = {
-      -- Make the server aware of Neovim runtime files
-      library = vim.api.nvim_get_runtime_file("", true),
-    },
-    telemetry = {
-        enable = false,
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+          enable = false,
+      },
     },
   },
 })
